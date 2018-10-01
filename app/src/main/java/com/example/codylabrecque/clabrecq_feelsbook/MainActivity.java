167e6d2870a2328072c48ce7d409a1f00ce5501a
clabrecq <clabrecq@ualberta.ca>
2018-10-01 14:34:40 -0600

    commit me

---
+++ app/src/main/java/com/example/codylabrecque/clabrecq_feelsbook/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.codylabrecque.clabrecq_feelsbook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    //Bundle bundle = getIntent().getExtras();
+    //int joy = bundle.getInt("joy");
+    private int joy = 0;
+    private int sadness = 0;
+    private int love = 0;
+    private int surprise = 0;
+    private int anger = 0;
+    private int fear = 0;
+    public static final String EXTRA_MESSAGE = "com.example.CLABRECQ-FeelsBook.MESSAGE";
+    //public static final String EMOTION = "com.example.CLABRECQ-FeelsBook.MESSAGE";
+    private List numbers = new ArrayList<String>();
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_activity);
+
+        Spinner dropdown = findViewById(R.id.spinner);
+        String[] items = new String[]{"Joy","Sadness","Love","Surprise","Anger","Fear"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+
+        loadFromFile();
+        updateCount();
+
+
+
+        }
+    private List<String> loadFromFile() {
+        List<String> history = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(addActivity.FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            String line = in.readLine();
+
+            while (line != null) {
+                history.add(line);
+                String[] numbers = line.split("--");
+                //String test = numbers[0];
+                if (numbers[0].equals("Joy")){
+                    joy++;
+                }
+                if (numbers[0].equals("Surprise")){
+                    surprise++;
+                }
+                if (numbers[0].equals("Sadness")){
+                    sadness++;
+                }
+                if (numbers[0].equals("Fear")){
+                    fear++;
+                }
+                if (numbers[0].equals("Anger")){
+                    anger++;
+                }
+                if (numbers[0].equals("Love")){
+                    love++;
+                }
+                line = in.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return history;
+    }
+
+
+    public void updateCount(){
+        String joyText = "Joy" + " - " + joy;
+        TextView joyView = findViewById(R.id.textView2);
+        joyView.setText(joyText);
+        String surpriseText = "Surprise" + " - " + surprise;
+        TextView surpriseView = findViewById(R.id.textView7);
+        surpriseView.setText(surpriseText);
+        String loveText = "Love" + " - " + love;
+        TextView loveView = findViewById(R.id.textView8);
+        loveView.setText(loveText);
+        String sadnessText = "Sadness" + " - " + sadness;
+        TextView sadnessView = findViewById(R.id.textView4);
+        sadnessView.setText(sadnessText);
+        String angerText = "Anger" + " - " + anger;
+        TextView angerView = findViewById(R.id.textView5);
+        angerView.setText(angerText);
+        String fearText = "Fear" + " - " + fear;
+        TextView fearView = findViewById(R.id.textView6);
+        fearView.setText(fearText);
+    }
+
+
+
+    public void onClick(View view) {
+
+        Date currentTime = Calendar.getInstance().getTime();
+        Spinner dropdown = findViewById(R.id.spinner);
+        String dropText = dropdown.getSelectedItem().toString();
+        Intent intent = new Intent(getApplicationContext(), addActivity.class);
+        //Intent history = new Intent(getApplicationContext(), addActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        String MadisonSquareGarden = dropText +"-"+ message +"-"+ currentTime;
+        intent.putExtra(EXTRA_MESSAGE, MadisonSquareGarden);
+        if (dropText == "Joy"){
+            this.joy ++;
+            String joyText = dropText + " - " + joy;
+            TextView joyView = findViewById(R.id.textView2);
+            joyView.setText(joyText);
+
+        }
+
+
+        if (dropText == "Surprise"){
+            this.surprise ++;
+            String surpriseText = dropText + " - " + surprise;
+            TextView surpriseView = findViewById(R.id.textView7);
+            surpriseView.setText(surpriseText);
+
+        }
+
+
+        if (dropText == "Love"){
+            this.love ++;
+            String loveText = dropText + " - " + love;
+            TextView loveView = findViewById(R.id.textView8);
+            loveView.setText(loveText);
+
+        }
+
+
+        if (dropText == "Sadness"){
+            this.sadness ++;
+            String sadnessText = dropText + " - " + sadness;
+            TextView sadnessView = findViewById(R.id.textView4);
+            sadnessView.setText(sadnessText);
+
+        }
+
+
+        if (dropText == "Anger"){
+            this.anger ++;
+            String angerText = dropText + " - " + anger;
+            TextView angerView = findViewById(R.id.textView5);
+            angerView.setText(angerText);
+
+        }
+
+
+        if (dropText == "Fear"){
+            this.fear ++;
+            String fearText = dropText + " - " + fear;
+            TextView fearView = findViewById(R.id.textView6);
+            fearView.setText(fearText);
+
+        }
+
+
+
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/codylabrecque/clabrecq_feelsbook/addActivity.java
@@ -0,0 +1,114 @@
+package com.example.codylabrecque.clabrecq_feelsbook;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class addActivity extends AppCompatActivity {
+    public static String FILENAME = "data";
+    private List text = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        saveInFile(message, new Date(System.currentTimeMillis()));
+
+        this.text = loadFromFile();
+        Button add_button = findViewById(R.id.button);
+        //TextView bodyText = findViewById(R.id.textView);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent add_emotion = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(add_emotion);
+            }
+        });
+
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        //String dropText = intent.getStringExtra(addActivity.EMOTION);
+        //TextView textView = findViewById(R.id.textView);
+        //String MadisonSquareGarden = message + dropText;
+        //textView.setText(message);
+        //textView.setText(dropText);
+        //textView.setMovementMethod(new ScrollingMovementMethod());
+        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1, text);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        //saveInFile(message, new Date(System.currentTimeMillis()));
+        //finish();
+
+
+    }
+    private List <String> loadFromFile() {
+        List<String> history = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            String line = in.readLine();
+            while (line != null) {
+                history.add(line);
+                line = in.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return history;
+    }
+    private void saveInFile(String text, Date date) {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_APPEND);
+            fos.write(new String( text + "\n").getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    }
+
+
+
+
+
+
